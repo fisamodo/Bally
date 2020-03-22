28accfddb851487e5d0049029e1639738509e0b7
fisamodo <fisamodo@vsite.hr>
2020-03-22 14:13:42 -0700

    Added gamestatebase, will try to make rules tommorow

---
+++ Source/Bally/BallyGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "BallyGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLY_API ABallyGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
